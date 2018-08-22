ee5f23d082099e1e75fa0cd4d2b1b46fdd792db7
Chris Wood <chris.wood@tescobank.com>
2018-08-22 16:54:45 +0100

    Added post and locals for score and questions

--- server.js
+++ server.js
@@ -6,24 +6,56 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const questions = [
+app.locals.score = 0;
+app.locals.questions = [
     {
-        id: 1,
         question: "What is DevOps?",
         answer: "culture"
     },
     {
-        id: 2,
         question: "What is cloud?",
         answer: "computers"
+    },
+    {
+        question: "Windows or Linux?",
+        answer: "Linux"
     }
 ]
 
 app.get('/', function(req, res){
+    let questions = app.locals.questions;
     res.render('index', {
         result: null,
-        question: questions[0]["question"],
-        question_id: questions[0]["id"]
+        question: questions[0]['question'],
+        question_id: 0
+    });
+})
+
+app.post('/', function(req, res){
+    let questions = app.locals.questions;
+    let answer = req.body.answer;
+    let question_id = req.body.question_id;
+    let next_question_id = Number(question_id) + 1;
+
+    if (answer == questions[question_id]['answer']) {
+        app.locals.score = Number(app.locals.score) + 1;
+        var result = "CORRECT!";
+    } else {
+        var result = "INCORRECT!";
+    }
+
+    if (questions.length == next_question_id) {
+        var result = `END OF QUIZ! You scored ${app.locals.score} out of ${questions.length}`;
+        var next_question = null;
+        app.locals.score = 0;
+    } else {
+        var next_question = questions[next_question_id]['question'];
+    }
+
+    res.render('index', {
+        result: result,
+        question: next_question,
+        question_id: next_question_id
     });
 })
 
